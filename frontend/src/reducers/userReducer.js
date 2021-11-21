4d35a25cfa010e9c88a8141576bb8366a44d01ed
Will Sanders <williamsanders81@gmail.com>
2021-11-21 10:58:59 -0800

    fixed local storage issues

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -8,6 +8,8 @@ export default function userReducer(state = {}, action) {
 			window.localStorage.setItem("id", action.id)
 			return { ...state, user: action.username, id: action.id };
 		case LOGOUT:
+			window.localStorage.removeItem("user")
+			window.localStorage.removeItem("id")
 			return { ...state, user: null, id:null };
 		default:
 			return state;
